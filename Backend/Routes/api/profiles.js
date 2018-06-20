63fbcaf1c25eb1f7a343ab45343ab932ba892d5d
prabhath782 <prabhath.teja.k@gmail.com>
2018-06-19 21:59:36 -0500

    refactor: add new route

--- Backend/Routes/api/profiles.js
+++ Backend/Routes/api/profiles.js
@@ -5,4 +5,8 @@ router.get('/test',(req,res)=>{
     res.json({msg:"Users profiles"});
 })
 
+router.get('/test/profile',()=>{
+    res.json({hello:"this is new route"});
+})
+
 export default router;
