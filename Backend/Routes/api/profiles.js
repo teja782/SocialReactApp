a5d342987661498d8f3ff54f1e31a22c877bf413
prabhath782 <prabhath.teja.k@gmail.com>
2018-06-20 20:38:37 -0500

    feat: add route for hello

--- Backend/Routes/api/profiles.js
+++ Backend/Routes/api/profiles.js
@@ -5,17 +5,21 @@ router.get('/test',(req,res)=>{
     res.json({msg:"Users profiles"});
 })
 
-router.get('/test/profile',()=>{
+router.get('/test/profile',(res,req)=>{
     res.json({hello:"this is new route"});
 })
 
-router.post('test/add',()=>{
+router.post('test/add',(res, req)=>{
     res.json({this:"this is a check"});
 })
 
 
-router.post('test/add/12345',()=>{
+router.post('test/add/12345',(res, req)=>{
     res.json({this:"this is a check"});
+})
+
+router.all('new',(res, req)=>{
+    res.json({hello:"this is tek"});
 })
 
 export default router;
